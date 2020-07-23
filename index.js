3d00f27364d0248f2486e76f1c9ad4409527d27f
BrunoBaldissera <bruno.baldissera@usp.br>
2020-07-23 18:59:19 -0300

    bodyparser adicionado no index

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 var express = require("express");
 var mongoose = require("mongoose");
 var routes = require("./routes/routes");
-
-app.use('/api', apiRoutes);
+let bodyParser = require('body-parser');
 
 var app = express();
 
-mongoose.connect("mongodb://localhost:39725/pet-shop/", {
+// Configura o body-parser para lidar rquisições de post
+app.use(bodyParser.urlencoded({
+   extended: true
+}));
+
+app.use(bodyParser.json());
+
+const db_conn = "mongodb+srv://bbaldissera:1234@cluster0.tcafq.gcp.mongodb.net/test";
+mongoose.connect(db_conn, {
     useNewUrlParser: true,
 });
+var db = mongoose.connection;
 
 var port = process.env.PORT || 8080;
 
  app.get('/', function (req, res) {
-   res.json('Hello World!');
+   res.json('Hello World!!!');
  });
 //routes(app);
+
+app.use('/api', routes);
 
 app.listen(port, function () {
     console.log("Example app listening on port "+port+"!");
