f3e0ba5db4bcb0daddb6d1d707e65f95f6019867
ceneme <rceneme@gmail.com>
2020-07-26 02:17:37 -0300

    new scripts

---
+++ public/scripts/dados_animal.js
@@ -0,0 +1,9 @@
+var dados = new Vue({
+	el: '#container-pet',
+	data: {
+		nome_p: "",
+		id_p : "",
+		raca : "",
+        idade : ""
+    }
+});
---
+++ public/scripts/dados_produto.js
@@ -0,0 +1,8 @@
+var dados = new Vue({
+	el: '#container-pet',
+	data: {
+		nome_prod: "",
+		id_prod : "",
+		desc: ""
+    }
+});
